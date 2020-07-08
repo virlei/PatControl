c505a194c1986fd9f05027b2ae8ae4167a3be86f
virlei <virleijr@yahoo.com.br>
2020-07-07 21:18:11 -0300

    Equipamento - Entity Class

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Equipamento;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Equipamento obj = new Equipamento(20,"Máquina de Escrever");
+		System.out.println(obj);
+	}
+
+}
