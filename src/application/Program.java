24ebd237ba1d1481317605c6f095fe1d85529a8e
virlei <virleijr@yahoo.com.br>
2020-07-08 12:06:45 -0300

    Patrimonio - Entity Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,17 @@
 package application;
 
 import model.entities.Equipamento;
+import model.entities.Patrimonio;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Equipamento obj = new Equipamento(20,"Máquina de Escrever");
-		System.out.println(obj);
+		Equipamento obj = new Equipamento(20,"Teclado");
+		
+		Patrimonio pat = new Patrimonio (83, "Positivo", "Positivo", "ABNT-2", 1, obj);
+		
+		System.out.println(pat);
 	}
 
 }
